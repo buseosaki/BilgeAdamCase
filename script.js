ecbf4e005c03257673f0dadc80978b5684f498bf
Zelal Buse Kizildag <zelalkizildag98@gmail.com>
2023-06-02 13:44:39 +0300

    script.js added

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const cities = [
+      "Adana", "Adıyaman", "Afyonkarahisar", "Ağrı", "Amasya", "Ankara", "Antalya", "Artvin", "Aydın", "Balıkesir",
+      "Bilecik", "Bingöl", "Bitlis", "Bolu", "Burdur", "Bursa", "Çanakkale", "Çankırı", "Çorum", "Denizli", "Diyarbakır",
+      "Edirne", "Elazığ", "Erzincan", "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkâri", "Hatay",
+      "Isparta", "Mersin", "İstanbul", "İzmir", "Kars", "Kastamonu", "Kayseri", "Kırklareli", "Kırşehir", "Kocaeli",
+      "Konya", "Kütahya", "Malatya", "Manisa", "Kahramanmaraş", "Mardin", "Muğla", "Muş", "Nevşehir", "Niğde", "Ordu",
+      "Rize", "Sakarya", "Samsun", "Siirt", "Sinop", "Sivas", "Tekirdağ", "Tokat", "Trabzon", "Tunceli", "Şanlıurfa",
+      "Uşak", "Van", "Yozgat", "Zonguldak", "Aksaray", "Bayburt", "Karaman", "Kırıkkale", "Batman", "Şırnak", "Bartın",
+      "Ardahan", "Iğdır", "Yalova", "Karabük", "Kilis", "Osmaniye", "Düzce"
+    ];
+  
+    const datalist = document.getElementById('cityList');
+  
+    cities.forEach(city => {
+      const optionElement = document.createElement('option');
+      optionElement.value = city;
+      datalist.appendChild(optionElement);
+    });
+  
+    const searchInput = document.getElementById('searchInput');
+  
+    searchInput.addEventListener('input', () => { //etkilesim
+      const searchText = searchInput.value;
+      console.log(searchText); 
+    });
+  });
+  
